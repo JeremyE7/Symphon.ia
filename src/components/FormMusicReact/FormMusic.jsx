7d206e483f588ab5a33270ff0b84c9802dfb5288
Jeremy <encaladajeremy@gmail.com>
2024-08-06 02:44:09 -0500

    Cambio total a ultima hora de casi toda la vista de la página xd

--- src/components/FormMusicReact/FormMusic.jsx
+++ src/components/FormMusicReact/FormMusic.jsx
@@ -7,12 +7,12 @@ export const FormMusic = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault()
-        
+
         const formData = new FormData(event.target)
         const feeling = formData.get('feeling')
         const genre = formData.get('genre')
         const instrument = formData.get('instrument')
-        
+
         try {
             const response = await fetch('/api/createSound', {
                 method: 'POST',
@@ -43,17 +43,16 @@ export const FormMusic = () => {
                     <div className={styles.line}></div>
                     <div className={styles.line}></div>
                 </div>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="feeling">What kind of feeling would you like to express?</label>
-                    <input type="text" id="feeling" name="feeling" />
-                    <label htmlFor="genre">What genre would you like to apply for your music?</label>
-                    <input type="text" id="genre" name="genre" />
-                    <label htmlFor="instrument">What instrument would you like to use?</label>
-                    <input type="text" id="instrument" name="instrument" />
-                    <button type="submit">Submit</button>
-                </form>
-                <div>
-                    {sound && <p>{sound.toString()}</p>}
+                <div className={styles.music}>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="feeling">What kind of feeling would you like to express?</label>
+                        <input type="text" id="feeling" name="feeling" required />
+                        <button type="submit">Submit</button>
+                    </form>
+                    <article className={styles.musicPlayer}>
+                        AQUI VA UN REPRODUCTOR DE MUSICA XD
+                        {sound && <p>{sound.toString()}</p>}
+                    </article>
                 </div>
             </section>
         </>
